d720dc34b81977cdb4ecf7e51594328d653ed32e
Dian-quotes-trend <kakumba19@gmail.com>
2025-05-09 15:15:17 +0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { InstagramFeed } from "@/components/instagram-feed"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-white">
+      <InstagramFeed />
+    </main>
+  )
+}
